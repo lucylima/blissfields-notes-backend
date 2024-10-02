c5b6b52793b9a2b6aff206eeeff89fa307e92133
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-01 22:23:22 -0300

    feat: sequelize setup

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { Sequelize } from "sequelize";
+import "dotenv/config"
 
-const database = new PrismaClient()
+const database = new Sequelize(process.env.DATABASE_URL,{
+    dialect: "postgres",
+})
 
-export { database }
+const sequelizeAuthenticate = async () => {
+    try {
+        await database.sync()
+        console.log("database connection successful")
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export { database, sequelizeAuthenticate }
