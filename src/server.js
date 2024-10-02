daf09e9305235775a94bd8c42598de5b8672ff9a
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-10-01 22:27:53 -0300

    feat: adding user route and database connection function

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import { sequelizeAuthenticate } from './database/database.js'
 import 'dotenv/config'
+import userRouter from './routes/user.route.js'
 
 const port = process.env.PORT || 3000
 const server = express()
 
 server.use(cors())
+server.use(userRouter)
 
 server.listen(port, () => {
   console.log('running at port ' + port)
+  sequelizeAuthenticate()
 })
